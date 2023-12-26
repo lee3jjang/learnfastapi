f51d96a71308f38e04894434d914119b1169244c
Sangjin Lee <sjlee@kodebox.io>
2023-12-26 19:38:09 +0900

    Add Navigation

---
+++ frontend/src/components/Navigation.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { link } from "svelte-spa-router";
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+  <div class="container-fluid">
+    <a use:link class="navbar-brand" href="/">Pybo</a>
+    <button
+      type="button"
+      class="navbar-toggler"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a use:link class="nav-link" href="/user-create">회원가입</a>
+        </li>
+        <li class="nav-item">
+          <a use:link class="nav-link" href="/user-login">로그인</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
