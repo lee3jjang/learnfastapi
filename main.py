353e08c001d67875e6725469955feb6ff06e9de4
Sangjin Lee <sjlee@kodebox.io>
2023-12-26 14:03:05 +0900

    Add cors middleware to fastapi

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from pydantic import BaseModel
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/hello")
 def hello():
-    return {"mesasge": "안녕하세요 파이보"}
+    return {"message": "안녕하세요 파이보"}
 
 
 class Item(BaseModel):
